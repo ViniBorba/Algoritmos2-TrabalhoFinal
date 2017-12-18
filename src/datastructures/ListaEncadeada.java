8d11365afeafba1e18a809584fde244d2cee191b
Vinicius Borba <vinichius@gmail.com>
2017-12-18 10:48:24 -0200

    D Alterei a calsse Lista Ecadeada

--- src/datastructures/ListaEncadeada.java
+++ src/datastructures/ListaEncadeada.java
@@ -4,17 +4,26 @@ import java.io.FileReader;
 import java.util.Comparator;
 
 public class ListaEncadeada<T> {
+	
+	//acrescentar
 	public void append(T dado)
 	{}
+	
+	//Adiciona antes
 	public void addFirst(T dado)
 	{}
+	
+	//procura
 	public T search(T key, Comparator<T> cmp)
 	{
 		return null;
 	}
+	
+	//mostra na tela
 	public void printObjects()
 	{}
 	
+	//para ler arquivo eu acho
 	public static ListaEncadeada<model.Aluno> loadFromFile(FileReader arquivo)
 	{
 		return null;
